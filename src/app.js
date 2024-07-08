2cab0371a872f696059d348def0977745e384414
PatriciaAlEs <168550797+PatriciaAlEs@users.noreply.github.com>
2024-07-08 11:07:29 +0000

    generador PatriciaAlEs

--- src/app.js
+++ src/app.js
@@ -5,7 +5,37 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function random1(longitudArray) {
+  let randomQuien = Math.floor(Math.random() * longitudArray);
+  return randomQuien;
+}
+
+function generandoExcusas() {
+  const generaQuien = ["El perro ", "Mi abuela ", "El cartero ", "Mi Gato "];
+  const generaAccion = ["comió ", "arañó ", "perdió ", "rompió "];
+  const generaQue = ["mi trabajo ", "mi telefono ", "mi coche "];
+  const generaCuando = [
+    "despues de clase",
+    "cuando estoy durmiendo",
+    "mientras estoy durmiendo",
+    "durante mi comida",
+    "mientras estoy jugando"
+  ];
+
+  const indiceQuien = random1(generaQuien.length);
+  const indiceAccion = random1(generaAccion.length);
+  const indiceQue = random1(generaQue.length);
+  const indiceCuando = random1(generaCuando.length);
+
+  return (
+    generaQuien[indiceQuien] +
+    generaAccion[indiceAccion] +
+    generaQue[indiceQue] +
+    generaCuando[indiceCuando]
+  );
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("excuse").innerHTML = generandoExcusas();
+  console.log(generandoExcusas());
 };
